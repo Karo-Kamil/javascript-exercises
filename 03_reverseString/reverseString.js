833eedd778a053527b6be791cedb7d2eb02bb327
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-26 17:47:18 +0300

    Finish third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let reversedString = "";
+    let strLen = phrase.length;
+    for (let i = 1; i <= strLen; i++) {
+        reversedString += phrase.at(-i);
+    }
+    return reversedString
 };
 
 // Do not edit below this line
