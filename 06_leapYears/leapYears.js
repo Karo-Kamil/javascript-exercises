1b64a5347f5465801a000d8bcbb8cd3814ec3433
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-27 11:13:00 +0300

    Finish sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let numYear = +year;
+    if (!(numYear % 400)){
+        return true;
+    } else if (!(numYear % 100)) {
+        return false;
+    } else if (!(numYear % 4)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
