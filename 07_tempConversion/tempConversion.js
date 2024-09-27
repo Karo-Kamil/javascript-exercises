03ad1d3343b258ee54fcb16636734c740493aa74
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-27 11:25:16 +0300

    Finish seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(temp) {
+  let fahr = 0;
+  if (typeof +temp === "number"){
+    fahr = (+temp * 1.8) + 32;
+    fahr = Math.round(fahr * 10) / 10;
+  }
+  return fahr;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  let celc = 0;
+  if (typeof +temp === "number"){
+    celc = (+temp - 32) / 1.8;
+    celc = Math.round(celc * 10) / 10;
+  }
+  return celc;
 };
 
 // Do not edit below this line
