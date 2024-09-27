211e62f86a6faec926ef8fa7a76c987eb62604f8
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-27 10:50:09 +0300

    Finish fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    array = arguments[0];
+    for (let j = 0; j < array.length; j++) {
+        for (let i = 1; i < arguments.length; i++) {
+            let item = arguments[i];
+            
+             if (array[j] === item) {
+                array.splice(j, 1);
+                j = 0;
+            }
+        }
+       
+    }
+    return array;
 };
 
 // Do not edit below this line
