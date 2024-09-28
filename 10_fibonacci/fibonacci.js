1527189a9ee7d06efbc572626fb21019421d0ce2
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-28 21:14:01 +0300

    Finish tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let intReq = parseInt(a);
+    const fibSeq = [0, 1];
+    if (intReq < 0){
+        return "OOPS"
+    } else if (intReq > 0){
+        for (let i = 1; i < intReq; i++) {
+        fibSeq.push(fibSeq[fibSeq.length-1] + fibSeq[fibSeq.length-2]);
+    }
+    return fibSeq [fibSeq.length - 1];
+    } else {
+        return fibSeq[0];
+    }
+    
+    
 };
 
 // Do not edit below this line
