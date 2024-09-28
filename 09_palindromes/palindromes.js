8f0a7c2e0e768a34a5a6fb129c65770c016b636f
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-28 20:53:04 +0300

    Finish ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let cleanedString = (a.replace(/[^a-zA-Z0-9]/g, "")).toLowerCase();
+    let reversed = "";
+    for (let i = 1; i <= cleanedString.length; i++) {
+        reversed += cleanedString.at(-i);
+    }
+    //let splitStr = cleanedString.split("");
+    //let reversedSplit = splitStr.reverse();
+    //let cleanString = splitStr.join("");
+    //let reversed = reversedSplit.join("");
+    //console.log(cleanString + " " + reversed);
+    return cleanedString === reversed;
 
 };
 
