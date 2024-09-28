ce3d8e81091f952115bfa15b6b9a48c0a373d9f9
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-28 20:32:41 +0300

    finish eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
+const add = function(a, b) {
+  parseInt(a);
+  parseInt(b);
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  parseInt(a);
+  parseInt(b);
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(a) {
+  let sum = 0;
+  if (a.length) {
+    for (let i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+  }
+  return sum;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+  if (a.length) {
+    for (let i = 0; i < a.length; i++) {
+      result *= a[i];
+    }
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+  if (b) {
+    for (let i = 0; i < b; i++) {
+      result *= a;
+    }
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+let result = 1;
+if (a) {
+  for (let i = 1; i <= a; i++) {
+    result *= i;
+  }
+}
+	return result;
 };
 
 // Do not edit below this line
