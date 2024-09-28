dfbdc0007b70de436209e366443e55048b13dfb5
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-28 21:34:31 +0300

    Finish twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const sortedArr = a.sort((a, b) => {
+        //console.log(a);
+        if (a.yearOfDeath) {
+            if (b.yearOfDeath) {
+                return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+            } else {
+                return (2024 - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+            }
+        } else {
+            return (b.yearOfDeath - b.yearOfBirth) - (2024 - a.yearOfBirth);
+        }
+        
+    })
+    
+    return sortedArr[0];
 
 };
 
